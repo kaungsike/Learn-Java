709508390ca7464e334bcb37857b5bd35bfd20e5
kaungsike <kaungsike9@gmail.com>
2024-06-21 03:52:16 +0630

    Learn Java

--- JavaBasic/Person.java
+++ JavaBasic/Person.java
@@ -2,6 +2,31 @@ package JavaBasic;
 
 public class Person {
     public  static  void main(String[] args){
-        System.out.println("San Gyi Dr Pr");
+        Student aung = new Student();
+        aung.name = "Aung Aung";
+        aung.age = 22;
+
+        Student aye = new Student();
+        aye.name = "Aye Aye";
+        aye.age = 11;
+
+        aye.greet();
+        aung.greet();
+
+        System.out.println(aye.name + " is " + aye.age + " years old.");
     }
+}
+
+
+class Student {
+    String name;
+    int age;
+
+    void greet() {
+        System.out.println("------------------");
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age);
+        System.out.println("------------------");
+    }
+
 }
